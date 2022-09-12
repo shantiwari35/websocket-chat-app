35daba5d99579bba18fb8b3b3056873a0fc3c3c3
shantiwari35 <shantiwari35@gmail.com>
2022-09-12 20:22:59 +0530

    css changes

--- js/client.js
+++ js/client.js
@@ -3,12 +3,21 @@ const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector(".container");
 
+var audio = new Audio('ting.mp3');
 const append = (message, position) => {
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
     messageElement.classList.add('message');
     messageElement.classList.add(position);
+
+    messageElement.classList.add('box-shadow-3d');
     messageContainer.append(messageElement);
+    if (position == 'left') {
+        audio.play();
+        messageElement.classList.add('box-shadow-3d-left');
+    } else {
+        messageElement.classList.add('box-shadow-3d-right');
+    }
 }
 form.addEventListener('submit', (e) => {
     e.preventDefault();
